c6439300bd954aa3cf6e1b030a6402349960fa46
sugyan <sugi1982@gmail.com>
2015-11-20 19:34:14 +0900

    update

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,16 @@ var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 
 gulp.task('build', () => {
-    return gulp.src('src/main.js')
+    return gulp.src('src/js/*.js')
         .pipe(babel({ presets: ['es2015'] }))
-        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.init({ loadMaps: true }))
         .pipe(uglify())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('static/js'));
 });
 
 gulp.task('watch', () => {
-    gulp.watch('src/**/*.js', ['build']);
+    gulp.watch('src/js/*.js', ['build']);
 });
 
 gulp.task('default', ['build']);
